beaa481d4e304fe9feaa65ccc9c75354e0de9c15
tanishab <tanishabraganza@gmail.com>
2022-07-08 09:17:28 +0100

    Multiplu method added

--- Calculator.java
+++ Calculator.java
@@ -7,12 +7,18 @@ public class Calculator {
 	public static int sub(int firstNumber, int secondNumber) {
 		return firstNumber - secondNumber;
 	}
+
+	public static int mul(int firstNumber, int secondNumber) {
+		return firstNumber * secondNumber;
+	}
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Result on adding: 50 and 20 = " + add(50,20));
 		
 		System.out.println("Result on subtracting: 50 and 20 = " + sub(50,20));
+
+		System.out.println("Result on multiplication: 50 and 20 = " + mul(50,20));
 
 	}
 
