adb05a5ac7bab45f729580729042de78a60b928e
tanishab <tanishabraganza@gmail.com>
2022-07-08 09:03:40 +0100

    add and sub methods

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+
+	public static int add(int firstNumber, int secondNumber) {
+		return firstNumber + secondNumber;
+	}
+	
+	public static int sub(int firstNumber, int secondNumber) {
+		return firstNumber - secondNumber;
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Result on adding: 50 and 20 = " + add(50,20));
+		
+		System.out.println("Result on subtracting: 50 and 20 = " + sub(50,20));
+
+	}
+
+}
